c66c19a59d85291e0f3c7bf6ed4cceb3ea64ea7b
Oscar Da Silva <oscardasilva52@gmail.com>
2021-11-15 14:10:41 -0300

    segundo objeto

--- Bytebank/index.js
+++ Bytebank/index.js
@@ -11,4 +11,12 @@ clinete1.cpf = 45612378974;
 clinete1.agencia = 1001;
 clinete1.saldo = 20;
 
-console.log(clinete1);
+const clinete2 = new Cliente();
+clinete2.nome = "Maria";
+clinete2.cpf = 11122233309;
+clinete2.agencia = 1001;
+clinete2.saldo = 0;
+
+console.log(clinete1); 
+console.log(clinete2); 
+

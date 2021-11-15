6dc86fab5604d4f88cb8480cf869d6ec029b966f
Oscar Da Silva <oscardasilva52@gmail.com>
2021-11-15 14:06:59 -0300

    primeira class

---
+++ Bytebank/index.js
@@ -0,0 +1,14 @@
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+}
+
+const clinete1 = new Cliente();
+clinete1.nome = "Ricardo";
+clinete1.cpf = 45612378974;
+clinete1.agencia = 1001;
+clinete1.saldo = 20;
+
+console.log(clinete1);

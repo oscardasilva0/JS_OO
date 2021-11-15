ec529415d5a7da3173e5f9cc40b6f98dd4f8575b
Oscar Da Silva <oscardasilva52@gmail.com>
2021-11-15 14:33:27 -0300

    metodo sacar

--- Bytebank/index.js
+++ Bytebank/index.js
@@ -1,22 +1,43 @@
 class Cliente {
   nome;
   cpf;
+
+}
+
+class ContaCorrente {
   agencia;
   saldo;
+  sacar(valor){
+    if (this.saldo >= valor) {
+      this.saldo -= valor;
+    }
+  }
 }
 
 const clinete1 = new Cliente();
 clinete1.nome = "Ricardo";
 clinete1.cpf = 45612378974;
-clinete1.agencia = 1001;
-clinete1.saldo = 20;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+
+contaCorrenteRicardo.sacar(200)
+console.log(contaCorrenteRicardo.saldo);
+
+
 
 const clinete2 = new Cliente();
 clinete2.nome = "Maria";
 clinete2.cpf = 11122233309;
-clinete2.agencia = 1001;
-clinete2.saldo = 0;
+const contaCorrenteMaria = new ContaCorrente();
+contaCorrenteMaria.saldo = 0;
+contaCorrenteMaria.agencia = 1001;
+console.log(contaCorrenteMaria.saldo);
+contaCorrenteMaria.saldo = 100;
 
-console.log(clinete1); 
-console.log(clinete2); 
+
+console.log(clinete1);
+console.log(clinete2);
 
